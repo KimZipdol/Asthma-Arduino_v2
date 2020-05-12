c9ca63283aba7aef947454f8975fc44f1b806269
JimoonKim <wlansdl@gmail.com>
2020-05-12 19:12:17 +0900

    bluetooth working

--- sketch_mar30a/sketch_mar30a.ino
+++ sketch_mar30a/sketch_mar30a.ino
@@ -1,26 +1,40 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include <SoftwareSerial.h>
 #include "src/Omron_D6FPH.h"
 
 Omron_D6FPH mySensor;
 
+//블루투스 연결
+SoftwareSerial BTSerial(4, 5);
+
 void setup()
 {
   Serial.begin(115200);
-  Wire.begin();
+  /*
+  if(!BLE.begin())
+    Serial.println("BLE begin failed");
+    */
+  BTSerial.begin(9600);
+    
   mySensor.begin(MODEL_5050AD3);
 }
 
 void loop()
 {
   float pressure = mySensor.getPressure();
-  if (isnan(pressure))
+  /*if (isnan(pressure))
   {
     Serial.println("Error: Could not read pressure data");
   }
   else
   {
     Serial.println(pressure);
+  }*/
+
+  if(BTSerial.available())
+  {
+    BTSerial.write(pressure);
+    BTSerial.flush();
   }
 
 }

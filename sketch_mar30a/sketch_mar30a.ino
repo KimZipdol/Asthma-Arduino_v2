aeb131236499e740fc23a80e65298f3e857cab71
KimZipdol <wlansdl@gmail.com>
2020-05-13 18:13:27 +0900

    softserial

--- sketch_mar30a/sketch_mar30a.ino
+++ sketch_mar30a/sketch_mar30a.ino
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "src/Omron_D6FPH.h"
+#include <SoftwareSerial.h> // 블루투스 통신을 위한 SoftwareSerial 라이브러리를 불러온다. 
 
 Omron_D6FPH mySensor;
+SoftwareSerial BTSerial(4, 5); // SoftwareSerial(RX, TX)
 
 void setup()
 {
-  Serial.begin(115200);
-  Wire.begin();
-  mySensor.begin(MODEL_5050AD3);
+  BTSerial.begin(9600); 
+  Serial.begin(9600); 
 }
 
 void loop()
@@ -16,11 +17,11 @@ void loop()
   float pressure = mySensor.getPressure();
   if (isnan(pressure))
   {
-    Serial.println("Error: Could not read pressure data");
+    BTSerial.println("Error: Could not read pressure data");
   }
   else
   {
-    Serial.println(pressure);
+    BTSerial.println(pressure);
   }
 
 }
